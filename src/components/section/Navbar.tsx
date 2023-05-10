699605c32c4b7b11d6c2e24ca9c4775710d1accf
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-05-10 17:37:38 +0700

    feat : add sidebar

--- src/components/section/Navbar.tsx
+++ src/components/section/Navbar.tsx
@@ -1,8 +1,35 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, useDisclosure } from '@chakra-ui/react';
 import Logo from '@/assets/logo.svg';
 import Search from '@/assets/search.svg';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  Image,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
+
+const DATA_NAVBAR = [
+  {
+    name: 'About Us',
+  },
+  {
+    name: 'Puppies',
+  },
+  {
+    name: 'Process',
+  },
+  {
+    name: 'What We Do?',
+  },
+];
+
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box
       display="flex"
@@ -19,7 +46,7 @@ const Navbar = () => {
         alignItems="center"
         px={{ base: '20px', sm: '40px', lg: '150px' }}
       >
-        <img src={Logo} alt="Logo" />
+        <Image src={Logo} alt="Logo" />
         <Flex
           fontSize="14px"
           justifyContent="space-between"
@@ -29,10 +56,11 @@ const Navbar = () => {
             md: 'flex',
           }}
         >
-          <Text cursor="pointer">About Us</Text>
-          <Text cursor="pointer">Puppies</Text>
-          <Text cursor="pointer">Process</Text>
-          <Text cursor="pointer">What We Do?</Text>
+          {DATA_NAVBAR.map((data, key) => (
+            <Text cursor="pointer" key={key}>
+              {data.name}
+            </Text>
+          ))}
         </Flex>
         <Flex
           justifyContent="space-between"
@@ -46,10 +74,36 @@ const Navbar = () => {
           <Text as="button">Account</Text>
           <img src={Search} alt="search" />
         </Flex>
-        <Box display={{ md: 'none' }}>
+        <Box display={{ md: 'none' }} onClick={onOpen} as="button">
           <RxHamburgerMenu size={25} />
         </Box>
       </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Image src={Logo} alt="Logo" />
+          </DrawerHeader>
+          <DrawerBody display="flex" flexDirection="column" gap={3}>
+            {DATA_NAVBAR.map((data, key) => (
+              <Text
+                key={key}
+                cursor="pointer"
+                as="button"
+                textAlign="center"
+                _hover={{
+                  bgColor: 'brand.brown',
+                  color: 'white',
+                  py: 2,
+                }}
+              >
+                {data.name}
+              </Text>
+            ))}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 };
