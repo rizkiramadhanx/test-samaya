7b114f31612502847587faa57daf71592c9445fb
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-05-10 09:18:28 +0700

    feat : add navbar

--- src/components/section/Navbar.tsx
+++ src/components/section/Navbar.tsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import Logo from '@/assets/logo.svg';
-
+import Search from '@/assets/search.svg';
+import { RxHamburgerMenu } from 'react-icons/rx';
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-[1280px] px-[30px] sm:px-[70px] md:[110px] m-auto">
-        <div className="flex items-center justify-between h-[100px]">
-          <div className="flex items-center">
-            <img src={Logo} width={100} />
-            <ul className="flex items-center ml-[70px] gap-x-10">
-              <li>Home</li>
-              <li>Learnings</li>
-              <li>About Us</li>
-              <li>Testimonial</li>
-            </ul>
-          </div>
-          <div>
-            <button>Login</button>
-            <button>Sign Up</button>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Box
+      display="flex"
+      justifyContent="center"
+      shadow="sm"
+      height={{ base: '70px', lg: '95px' }}
+    >
+      <Flex
+        color="brand.brown"
+        justify="space-between"
+        maxWidth={1440}
+        width="full"
+        height="full"
+        alignItems="center"
+        px={{ base: '20px', sm: '40px', lg: '150px' }}
+      >
+        <img src={Logo} alt="Logo" />
+        <Flex
+          fontSize="14px"
+          justifyContent="space-between"
+          gap="54px"
+          display={{
+            base: 'none',
+            md: 'flex',
+          }}
+        >
+          <Text cursor="pointer">About Us</Text>
+          <Text cursor="pointer">Puppies</Text>
+          <Text cursor="pointer">Process</Text>
+          <Text cursor="pointer">What We Do?</Text>
+        </Flex>
+        <Flex
+          justifyContent="space-between"
+          gap="40px"
+          display={{
+            base: 'none',
+            md: 'flex',
+          }}
+        >
+          <Text as="button">Login</Text>
+          <Text as="button">Account</Text>
+          <img src={Search} alt="search" />
+        </Flex>
+        <Box display={{ md: 'none' }}>
+          <RxHamburgerMenu size={25} />
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
